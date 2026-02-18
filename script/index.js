0184313c7a9e061b6a664fe8103771440646e4db
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-02-18 17:45:07 +0600

    <fix> fix the design in whysection

--- script/index.js
+++ script/index.js
@@ -1,1 +1,9 @@
+const url = "https://fakestoreapi.com/products";
 
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((element) => {
+      console.log(element.image);
+    });
+  });
